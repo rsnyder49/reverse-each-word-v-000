8c7ef8da63b87bc4c72a902c65a46008381da225
Robert Snyder <r.snyder49@gmail.com>
2018-08-09 20:20:03 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,10 @@ def reverse_each_word(string)
   end 
   return reverse_array.join(" ")
 end 
+
+def reverse_each_word(string)
+  array = string.split 
+  array.collect do |word| 
+    word.reverse 
+  end
+end 
